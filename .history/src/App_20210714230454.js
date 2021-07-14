bba08a7ae52c1b4fc578c55604ba47a337a80352
Hyperion <radoslaw.lewandowski10@gmail.com>
2021-07-14 23:13:05 +0200

    Add prop

--- .history/src/App_20210714230454.js
+++ .history/src/App_20210714230454.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([" Todo 1 ", " Todo 2 "]);
+  const [todos, setTodos] = useState(["Todo 1", "Todo 2"]);
   return (
     <>
-      <TodoList todoList={todos} />
+      <TodoList />
       <input type="text" />
       <button>Add Todo</button>
       <button>Clear Completed</button>
---
+++ .history/src/TodoList_20210714230617.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TodoList({ todoList }) {
+  return <div>Hello</div>;
+}
