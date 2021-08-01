22206e1a6b8196806020cee0cb04f8b6898eb792
Hyperion <radoslaw.lewandowski10@gmail.com>
2021-08-01 20:55:19 +0200

    Add todo filter

--- .history/src/App_20210801203629.js
+++ .history/src/App_20210801203629.js
@@ -43,7 +43,7 @@ function App() {
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed</button>
-      <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
+      <div>{todos.filter((todo) => !todo.complete)} left to do</div>
     </>
   );
 }
